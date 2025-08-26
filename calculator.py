eecccc7a346c6a313252eba877451f0b4a7cec57
Christine Hillebrand <chillebrand0822@gmail.com>
2025-08-25 21:48:14 -0400

    Add user interface

--- calculator.py
+++ calculator.py
@@ -18,11 +18,29 @@ def request_input(which):
             print("Input " + which + ": ")
             x = float(input())
             return x
-        except TypeError:
+        except ValueError:
             print("Please input numeric decimal input only")
 
 def process_inputs(x, y, func):
     return func(x, y)
 
+def request_function():
+    while True:
+        try:
+            print("Input number to select function: [0] Add, [1] Subtract, [2] Multiply, [3] Divide")
+            sel = int(input())
+            if sel == 0:
+                return add
+            elif sel == 1:
+                return subtract
+            elif sel == 2:
+                return multiply
+            elif sel == 3:
+                return divide
+        except ValueError:
+            print("You must input a number")
+
+func = request_function()
 x = request_input("x")
 y = request_input("y")
+print("Answer: ", process_inputs(x, y, func))
